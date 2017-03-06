3121a29305fb57f2ca291968ec7fb26f5ff9784c
dipe2003 <dipe.2003@hotmail.com>
2017-03-05 23:15:54 -0300

    Se completo Agregar/editar/remover actividades preventivas.

--- M.A.y.M.Web-war/src/java/com/dperez/maym/web/acciones/preventivas/ActividadesAP.java
+++ M.A.y.M.Web-war/src/java/com/dperez/maym/web/acciones/preventivas/ActividadesAP.java
@@ -45,6 +45,7 @@ public class ActividadesAP implements Serializable {
     private FacadeLectura fLectura;
     
     private Accion AccionSeleccionada;
+    private int IdActividadEditar;
     
     private Map<Integer, Usuario> ListaUsuariosEmpresa;
     
@@ -73,6 +74,7 @@ public class ActividadesAP implements Serializable {
     public int getResponsableActividad() {return ResponsableActividad;}
     
     public Accion getAccionSeleccionada() {return AccionSeleccionada;}
+    public int getIdActividadEditar() {return IdActividadEditar;}
     
     //  Setters
     public void setListaUsuariosEmpresa(Map<Integer, Usuario> ListaUsuariosEmpresa) {this.ListaUsuariosEmpresa = ListaUsuariosEmpresa;}
@@ -91,6 +93,7 @@ public class ActividadesAP implements Serializable {
     public void setDescripcionActividad(String DescripcionActividad) {this.DescripcionActividad = DescripcionActividad;}
     public void setResponsableActividad(int ResponsableActividad) {this.ResponsableActividad = ResponsableActividad;}
     
+    public void setIdActividadEditar(int IdActividadEditar) {this.IdActividadEditar = IdActividadEditar;}
     //  Metodos
     /**
      * Agrega una actividad a la accion preventiva.
@@ -99,10 +102,9 @@ public class ActividadesAP implements Serializable {
      * @throws java.io.IOException
      */
     public void agregarActividad() throws IOException{
-        int id;
-        if((id = fDatos.AgregarActividadPreventiva(AccionSeleccionada.getId(), FechaEstimadaImplementacionActividad, DescripcionActividad,
-                ResponsableActividad))<0){
-            FacesContext.getCurrentInstance().addMessage("form_agregar_actividades:btn_agregar_actividad_mejora", new FacesMessage(SEVERITY_FATAL, "No se pudo agregar Actividad", "No se pudo agregar Actividad" ));
+        if(fDatos.AgregarActividadPreventiva(AccionSeleccionada.getId(), FechaEstimadaImplementacionActividad, DescripcionActividad,
+                ResponsableActividad)<0){
+            FacesContext.getCurrentInstance().addMessage("form_agregar_actividades:btn_agregar_actividad_preventiva", new FacesMessage(SEVERITY_FATAL, "No se pudo agregar Actividad", "No se pudo agregar Actividad" ));
             FacesContext.getCurrentInstance().renderResponse();
         }else{
             // redirigir a la edicion de la accion correctiva.
@@ -112,27 +114,37 @@ public class ActividadesAP implements Serializable {
     }
     
     /**
-     * Remueve la actividad preventiva de la lista de actividades si la fecha de implementacion no esta definida.
      * Remueve la actividad de la accion preventiva.
      * Se persisten los cambios.
      * Muestra un mensaje de error si no se pudo completar correctamente.
-     * @param IdActividadPreventiva
+     * @throws java.io.IOException
      */
-    public void removerActividadPreventiva(int IdActividadPreventiva){
-        if(IdActividadPreventiva!=0 && ListaActividades.get(IdActividadPreventiva).getFechaImplementacion() == null){
-            int res;
-            if((res = fDatos.RemoverActividadPreventiva(AccionSeleccionada.getId(), IdActividadPreventiva))> 0){
-                ListaActividades.remove(res);
-            }else{
-                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(SEVERITY_FATAL, "No se pudo quitar la Actividad", "No se pudo quitar la Actividad" ));
-                FacesContext.getCurrentInstance().renderResponse();
-            }
-        }else{
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(SEVERITY_FATAL, "No se puede quitar porque ya fue implementada", "No se puede quitar porque ya fue implementada" ));
+    public void removerActividad() throws IOException{
+        if(fDatos.RemoverActividadPreventiva(AccionSeleccionada.getId(), IdActividadEditar)< 0){
+            FacesContext.getCurrentInstance().addMessage("form_agregar_actividades:btn_remover_actividad", new FacesMessage(SEVERITY_FATAL, "No se pudo quitar la Actividad", "No se pudo quitar la Actividad" ));
             FacesContext.getCurrentInstance().renderResponse();
+        }else{
+            // redirigir a la edicion de la accion de mejora.
+            String url = FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath();
+            FacesContext.getCurrentInstance().getExternalContext().redirect(url+"/Views/Acciones/Preventivas/EditarAccionPreventiva.xhtml?id="+AccionSeleccionada.getId());
         }
     }
     
+    /**
+     * Guarda los cambios realizados en la actividad y los persiste.
+     * Se redirige a la vista de editar la accion preventiva correspondiente si se guardo, de lo contrario muestra un mensaje.
+     * @throws IOException
+     */
+    public void guardarActividad() throws IOException{
+        if(fDatos.EditarActividad(IdActividadEditar, DescripcionActividad, ResponsableActividad, FechaEstimadaImplementacionActividad) < 0){
+            FacesContext.getCurrentInstance().addMessage("form_agregar_actividades:btn_guardar_actividad", new FacesMessage(SEVERITY_FATAL, "No se pudo guardar Actividad", "No se pudo guardar Actividad" ));
+            FacesContext.getCurrentInstance().renderResponse();
+        }else{
+            // redirigir a la edicion de la accion correctiva.
+            String url = FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath();
+            FacesContext.getCurrentInstance().getExternalContext().redirect(url+"/Views/Acciones/Mejoras/EditarAccionMejora.xhtml?id="+AccionSeleccionada.getId());
+        }
+    }
     
     //  Inicializacion del bean
     @PostConstruct
@@ -142,17 +154,27 @@ public class ActividadesAP implements Serializable {
         // recuperar el id para llenar datos de la accion preventiva y el resto de las propiedades.
         int idAccion = 0;
         idAccion = Integer.parseInt(request.getParameter("id"));
+        try{
+            IdActividadEditar = Integer.parseInt(request.getParameter("editar"));
+        }catch(Exception ex){
+            IdActividadEditar = 0;
+        }
         if(idAccion != 0){
             AccionSeleccionada = (Preventiva) fLectura.GetAccion(idAccion);
-            //  Actividades Preventivas
-            ListaActividades = new HashMap<>();
-            if(!((Preventiva)AccionSeleccionada).getActividades().isEmpty()){
-                List<Actividad> actividades = ((Preventiva)AccionSeleccionada).getActividades();
-                for(Actividad actividad: actividades ){
-                    ListaActividades.put(actividad.getIdActividad(), actividad);
+            if(IdActividadEditar > 0){
+                //  Actividades Preventivas
+                ListaActividades = new HashMap<>();
+                if(!((Preventiva)AccionSeleccionada).getActividades().isEmpty()){
+                    List<Actividad> actividades = ((Preventiva)AccionSeleccionada).getActividades();
+                    for(Actividad actividad: actividades ){
+                        ListaActividades.put(actividad.getIdActividad(), actividad);
+                    }
                 }
+                Actividad actividad = ListaActividades.get(IdActividadEditar);
+                FechaEstimadaImplementacionActividad = actividad.getFechaEstimadaImplementacion();
+                DescripcionActividad =  actividad.getDescripcion();
+                ResponsableActividad = actividad.getResponsableImplementacion().getId();
             }
-            
             //  Usuarios
             this.ListaUsuariosEmpresa = new HashMap<>();
             Empresa empresa = (Empresa) request.getSession().getAttribute("Empresa");
